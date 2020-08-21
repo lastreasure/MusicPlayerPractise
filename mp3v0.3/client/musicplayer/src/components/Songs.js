30ed53d48cf459e5dc2e497b3865847d0be11b7d
lastreasure <letitia.treasure@vodafone.com>
2020-08-21 16:03:39 +0100

    list

--- mp3v0.3/client/musicplayer/src/components/Songs.js
+++ mp3v0.3/client/musicplayer/src/components/Songs.js
@@ -2,18 +2,30 @@ import React, { Component } from 'react'
 
 class Songs extends Component {
 
+    constructor(props) {
+        super(props);
+        this.state = {
+            songs: []
+        }
+    }
+
     componentDidMount() {
-        fetch('http://localhost:5000/songs', { headers: {
-            'Access-Control-Allow-Origin':'*'
-        }})
+        fetch('http://localhost:5000/songs')
         .then(res => res.json())
-        .then(data => console.log(data))
+        .then(data => this.setState({ songs: data }))
     }
 
     render() {
+        const songItems = this.state.songs.map( song => (
+            <div key={song._id}>
+                <h3> {song.title} </h3>
+                <p>{song.artist}</p>
+            </div>
+        ));
         return (
             <div>
                 <h1>Songs</h1>
+                { songItems }
             </div>
         )
     }
