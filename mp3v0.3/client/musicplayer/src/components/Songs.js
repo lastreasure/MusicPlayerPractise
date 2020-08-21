60b29b764c68df37b6e9f970f85adc32ed4eeadf
lastreasure <letitia.treasure@vodafone.com>
2020-08-21 10:30:51 +0100

    ignore

--- mp3v0.3/client/musicplayer/src/components/Songs.js
+++ mp3v0.3/client/musicplayer/src/components/Songs.js
@@ -2,11 +2,11 @@ import React, { Component } from 'react'
 
 class Songs extends Component {
 
-    // componentWillMount() {
-    //     fetch('https://localhost:5000/songs')
-    //     .then(res => res.json())
-    //     .then(songs => console.log(songs))
-    // }
+    componentDidMount() {
+        fetch('https://localhost:5000/songs')
+        .then(res => res.json())
+        .then(songs => console.log(songs))
+    }
     render() {
         return (
             <div>
