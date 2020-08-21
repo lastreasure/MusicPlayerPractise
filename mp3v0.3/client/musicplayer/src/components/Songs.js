ea5eca358ebe3ba5c7215d03c70ee01645101f72
lastreasure <letitia.treasure@vodafone.com>
2020-08-21 14:52:08 +0100

    CORS

--- mp3v0.3/client/musicplayer/src/components/Songs.js
+++ mp3v0.3/client/musicplayer/src/components/Songs.js
@@ -1,6 +1,15 @@
 import React, { Component } from 'react'
 
 class Songs extends Component {
+
+    componentDidMount() {
+        fetch('http://localhost:5000/songs', { headers: {
+            'Access-Control-Allow-Origin':'*'
+        }})
+        .then(res => res.json())
+        .then(data => console.log(data))
+    }
+
     render() {
         return (
             <div>
--- mp3v0.3/server/server.js
+++ mp3v0.3/server/server.js
@@ -3,6 +3,7 @@ require('dotenv').config() //establishing env variables
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 
 const songsRouter = require('./routes/songs');
 const playlistsRouter = require('./routes/playlists');
@@ -21,8 +22,19 @@ const db = mongoose.connection;
 db.on('error', (error) => console.log(error));
 db.once('open', () => console.log('Connected to MongoDB'));
 
+// Prevent CORS issues and allow client access to api 
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', '*');
+    if(req.method === "OPTION") {
+        res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET, UPDATE');
+        return res.statusCode(200).json({});
+    }
+    next();
+});
 
-app.use(express.json());
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
 app.use(songsRouter);
 app.use(playlistsRouter);
 
