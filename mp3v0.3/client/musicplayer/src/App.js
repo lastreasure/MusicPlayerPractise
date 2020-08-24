b1692c53842ead04136893a61f1d224fdd2411a2
lastreasure <letitia.treasure@vodafone.com>
2020-08-24 13:03:43 +0100

    ui

--- mp3v0.3/client/musicplayer/src/App.js
+++ mp3v0.3/client/musicplayer/src/App.js
@@ -2,13 +2,12 @@ import React from 'react';
 import './App.css';
 import Header from './components/Header';
 import ControlPanel from './components/Controls/ControlPanel'
-import {Typography} from '@material-ui/core';
 
 
 function App()  {
     return (
         <div className="App">
-          <Typography>hello</Typography>
+
             <Header />
             <hr />
             <ControlPanel/>
---
+++ mp3v0.3/client2.0/musicplayer/src/components/SongImage.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react'
+
+class SongImage extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            song: [],
+            image: ''
+            
+        }
+    }
+
+    componentDidMount() {
+        const songId= "5f3f83fbaf74801987a8a8c1";
+        fetch(`http://localhost:5000/songs/${songId}`)
+        .then(res => res.json())
+        .then(data => this.setState({ song: data}))
+    }
+
+
+    render() {
+        // const songPic = this.state.song.map( song => (
+        //     song.imageSource
+        // ));     
+        
+        this.state.image = this.state.song.map( song => (
+            song.imageSource
+        ));  // use set state...
+        
+        console.log(this.state.image);
+
+        const image = this.state.image;
+        return (
+            <div>
+                <img src={process.env.PUBLIC_URL + `/${this.state.image}`}></img>
+            </div>
+        )
+    }
+}
+
+export default SongImage;
