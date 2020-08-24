741cb6b97c7f32b96347a0cd8be82f324ecdc5c1
lastreasure <letitia.treasure@vodafone.com>
2020-08-24 15:26:01 +0100

    play/pause

--- mp3v0.3/client2.0/musicplayer/src/components/ControlPanel.js
+++ mp3v0.3/client2.0/musicplayer/src/components/ControlPanel.js
@@ -1,49 +1,117 @@
-import React, {useState} from 'react'
+import React, {Component} from 'react'
 import Button from '@material-ui/core/Button'
 import PlayArrowIcon from '@material-ui/icons/PlayArrow';
 import PauseIcon from '@material-ui/icons/Pause';
 import SkipNextIcon from '@material-ui/icons/SkipNext';
 import SkipPreviousIcon from '@material-ui/icons/SkipPrevious';
 
-function ControlPanel (audioSource) {
+class ControlPanel extends Component {
 
-    // const [state, setState] = useState({isplaying: false})
-    // const isplaying = state.isplaying;
+    constructor(props) {
+        super(props);
+        this.state = { play: false }
+    }
 
+    componentWillReceiveProps() {
+        this.setState({ play: true });
+    };
 
-    // function play(audioFile) {
-    //     if(!isplaying) {
-    //         audioFile.play()
-    //         setState(!isplaying);
-    //         audioFile.pause()
-    //         setState(!isplaying);
-    //     }
+    // componentDidMount() {
+
     // }
 
-    console.log(audioSource.audioSource)
-    let audioFile = new Audio(audioSource.audioSource)
+    play = (audio) => {
+        if (this.state.play) {
+            audio.pause();
+            this.setState({ play: false });
+        } else {
+            audio.play();
+            this.setState({ play: true });
+        }
+    }
 
-    return (
+    render() {
 
-        <div>  
-            <Button variant="contained" color="primary">
-                <SkipPreviousIcon/>
-            </Button>
+        console.log(this.props.audioSource)
+        // let audioFile = new Audio(this.props.audioSource)
+        
 
-            <Button variant="contained" color="primary" onClick={() => audioFile.play()}>
-                <PlayArrowIcon/>
-            </Button>
+        return (
 
-            <Button variant="contained" color="primary" onClick={() => audioFile.pause()}>
-                <PauseIcon/>
-            </Button>
+            <div>  
+                <audio src={this.props.audioSource} ref={(audio) => {this.audio=audio}} />
 
-            <Button variant="contained" color="primary">
-                <SkipNextIcon/>
-            </Button>
+                <Button variant="contained" color="primary">
+                    <SkipPreviousIcon/>
+                </Button>
 
-        </div>
-    )
+                <Button variant="contained" color="primary" onClick={() => this.play(this.audio)}>
+                    {!this.state.play ? <PlayArrowIcon/> : <PauseIcon/> }
+                </Button>
+
+                <Button variant="contained" color="primary">
+                    <SkipNextIcon/>
+                </Button>
+
+            </div>
+        )
+    }
 }
 
 export default ControlPanel;
+
+
+
+
+
+
+
+// import React, {useState} from 'react'
+// import Button from '@material-ui/core/Button'
+// import PlayArrowIcon from '@material-ui/icons/PlayArrow';
+// import PauseIcon from '@material-ui/icons/Pause';
+// import SkipNextIcon from '@material-ui/icons/SkipNext';
+// import SkipPreviousIcon from '@material-ui/icons/SkipPrevious';
+
+// function ControlPanel (audioSource) {
+
+//     // const [state, setState] = useState({isplaying: false})
+//     // const isplaying = state.isplaying;
+
+
+//     // function play(audioFile) {
+//     //     if(!isplaying) {
+//     //         audioFile.play()
+//     //         setState(!isplaying);
+//     //         audioFile.pause()
+//     //         setState(!isplaying);
+//     //     }
+//     // }
+
+//     console.log(audioSource.audioSource)
+//     let audioFile = new Audio(audioSource.audioSource)
+
+//     return (
+
+//         <div>  
+//             <Button variant="contained" color="primary">
+//                 <SkipPreviousIcon/>
+//             </Button>
+
+//             <Button variant="contained" color="primary" onClick={() => audioFile.play()}>
+//                 <PlayArrowIcon/>
+//             </Button>
+
+//             <Button variant="contained" color="primary" onClick={() => audioFile.pause()}>
+//                 <PauseIcon/>
+//             </Button>
+
+//             <Button variant="contained" color="primary">
+//                 <SkipNextIcon/>
+//             </Button>
+
+//         </div>
+//     )
+// }
+
+// export default ControlPanel;
